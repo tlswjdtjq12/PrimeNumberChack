b60d0441f9ec44a6fcc2468b6ba84c5cc5d4c5d9
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-03-29 15:57:15 +0900

    Prime

--- PrimeNumberChack/ViewController.swift
+++ PrimeNumberChack/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt01: UITextField!
+    
+    @IBOutlet weak var lbl01: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnCheck(_ sender: Any) {
+        
+        
+        
+        var isPrime = true
+        let num = Int(txt01.text!)
+        
+        if num == 1 {
+            isPrime = false
+        }
+        
+        if num != 1  && num != 2 {
+            for i in 2 ..< num! {
+                if num! % i == 0 {
+                    isPrime = false
+                }
+            }
+        }
+        
+        if isPrime == true {
+            lbl01.text = "prime number"
+        } else{
+            lbl01.text = "Not prime number"
+        }
+    
+    }
+    
+    
+    @IBAction func btnRe(_ sender: Any) {
+        lbl01.text = ""
+        txt01.text = ""
+    }
+    
+    
 }
 
